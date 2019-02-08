30b58c9674774bd55b723cf5490a5eb24f3c2c11
Sergey <xxsmotur@gmail.com>
2019-02-08 19:20:10 +0500

    Add node file

---
+++ node.go
@@ -0,0 +1,29 @@
+package merkle
+
+import (
+	"errors"
+	"fmt"
+	"hash"
+)
+
+var errNewNode = errors.New("unable to create a new node")
+
+// Node defines node of the Merkle tree
+type Node struct {
+	Hash  []byte
+	Left  *Node
+	Right *Node
+}
+
+// NewNode creates a new node at the tree
+func NewNode(h hash.Hash, block []byte) (Node, error) {
+	if h == nil || block == nil {
+		return Node{}, errNewNode
+	}
+	defer h.Reset()
+	_, err := h.Write(block[:])
+	if err != nil {
+		return Node{}, fmt.Errorf("unable write to hash: %v", err)
+	}
+	return Node{Hash: h.Sum(nil)}, nil
+}
